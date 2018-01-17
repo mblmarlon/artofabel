da6c893e6ff58449febdc50d012578bd5204a08f
Marlon Berdefy <berdefymarlon@yahoo.de>
2018-01-17 20:36:39 +0100

    categories with proper routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'pages/contact'
+  get 'pages/fashion'
+  get 'pages/still'
+  get 'pages/portrait'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
